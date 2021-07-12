44d70d2fbb3a598279d0f35dad58b6acc4c2c89f
dev_davis <sean.dev.davis@gmail.com>
2021-07-12 02:48:25 -0500

    got basics of the site working

--- src/Components/Charms/CharmsCards.js
+++ src/Components/Charms/CharmsCards.js
@@ -1,12 +1,27 @@
 // imports
 import React from 'react'
 
-function CharmsCards() {
-  return (
-    <div className="CharmsCards">
+import '../../css/bracelets.css'
 
+function charmsCards({charm}) {
+
+  function setPrice() {
+    if(charm.set) {
+      return <p>Set: ${charm.set}</p>
+    } else {
+      return null
+    }
+  }
+
+  return (
+    <div className="col item-card">
+      <h6 className="item-name">{charm.name}</h6>
+      <img src={charm.avi} alt="item-pic" className="item-image" />
+      <p className="single-price">Single: ${charm.price}</p>
+      <p className="single-price">{setPrice()}</p>
+      {}
     </div>
   )
 }
 
-export default CharmsCards
+export default charmsCards
--- src/Components/Necklaces/NecklaceCards.js
+++ src/Components/Necklaces/NecklaceCards.js
@@ -1,12 +1,25 @@
 // imports
-import React from 'react'
+import React from "react";
 
-function NecklacesCards() {
+import "../../css/bracelets.css";
+
+function NecklacesCards({ necklace }) {
+  function setPrice() {
+    if (necklace.set) {
+      return <p>Set: ${necklace.set}</p>;
+    } else {
+      return null;
+    }
+  }
+
   return (
-    <div className="NecklacesCards">
-
+    <div className="col item-card">
+      <h6 className="item-name">{necklace.name}</h6>
+      <img src={necklace.avi} alt="item-pic" className="item-image" />
+      <p className="single-price">Single: ${necklace.price}</p>
+      <p className="single-price">{setPrice()}</p>
     </div>
-  )
+  );
 }
 
-export default NecklacesCards
+export default NecklacesCards;
